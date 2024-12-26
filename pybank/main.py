79ff1b5695557ef3bf5e594a4b3c1c672307f164
Azunti <114590551+Azunti@users.noreply.github.com>
2024-12-26 16:42:45 -0600

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,4 +1,23 @@
 import pandas as pd
 from pathlib import Path
-raw_df = pd.read_csv(Path("../Resources/budget_data.csv"))
-raw_df.head()
+raw = pd.read_csv(Path("../Resources/budget_data.csv"))
+raw.head()
+
+print("Financial Analysis")
+print("----------------------------")
+months = len(raw['Date'])
+print(f"Total Months: {months}")
+
+net = raw['Profit/Losses'].sum()
+print(f"Total: ${net}")
+
+aver = raw['Profit/Losses'].mean()
+print(f"Average Change: ${aver}")
+
+inpl = raw['Profit/Losses'].max()
+inmo = raw.loc[raw['Profit/Losses'].idxmax(), 'Date']
+print(f"Greatest Increase in Profits: {inmo} (${inpl})")
+
+depl = raw['Profit/Losses'].min()
+demo = raw.loc[raw['Profit/Losses'].idxmin(), 'Date']
+print(f"Greatest Decrease in Profits: {demo} (${depl})")
